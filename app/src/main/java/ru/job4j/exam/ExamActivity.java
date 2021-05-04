38dcab5f568b30d0ecf1ed588fdffa448da735b8
vladimirKutyavin <kva.devops@gmail.com>
2021-05-04 15:17:33 +0400

    2. MVC. Создания события и получение данных от пользователя. [#526031]

--- app/src/main/java/ru/job4j/exam/ExamActivity.java
+++ app/src/main/java/ru/job4j/exam/ExamActivity.java
@@ -5,16 +5,109 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ExamActivity extends AppCompatActivity {
 
     private static final String TAG = "ExamActivity";
     private static int counterTurn = 0;
+    private final List<Question> question = Arrays.asList(
+            new Question(
+                    1, "How many primitive variables does Java have?",
+                    Arrays.asList(
+                            new Option(1, "1.1"), new Option(2, "1.2"),
+                            new Option(3, "1.3"), new Option(4, "1.4")
+                    ), 4
+            ),
+            new Question(
+                    2, "What is Java Virtual Machine?",
+                    Arrays.asList(
+                            new Option(1, "2.1"), new Option(2, "2.2"),
+                            new Option(3, "2.3"), new Option(4, "2.4")
+                    ), 4
+            ),
+            new Question(
+                    3, "What is happen if we try unboxing null?",
+                    Arrays.asList(
+                            new Option(1, "3.1"), new Option(2, "3.2"),
+                            new Option(3, "3.3"), new Option(4, "3.4")
+                    ), 4
+            )
+    );
+    private int position = 0;
+    private Map<Integer, Integer> result = new HashMap<>();
+
+    private void fillForm() {
+        findViewById(R.id.previous).setEnabled(position != 0);
+        findViewById(R.id.next).setEnabled(position != question.size() - 1);
+        final TextView text = findViewById(R.id.question);
+        Question question = this.question.get(this.position);
+        text.setText(question.getText());
+        RadioGroup variants = findViewById(R.id.variants);
+        for (int index = 0; index != variants.getChildCount(); index++) {
+            RadioButton button = (RadioButton) variants.getChildAt(index);
+            Option option = question.getOptions().get(index);
+            button.setId(option.getId());
+            button.setText(option.getText());
+        }
+    }
+
+    private void showAnswer() {
+        RadioGroup variants = findViewById(R.id.variants);
+        int id = variants.getCheckedRadioButtonId();
+        Question question = this.question.get(this.position);
+        result.put(position, id);
+        Toast.makeText(
+                this, "Your answer is " + id + ", correct is " + question.getAnswer(),
+                Toast.LENGTH_SHORT
+        ).show();
+        Toast.makeText(this, "result map: " + result, Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.fillForm();
+        Button next = findViewById(R.id.next);
+        next.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        RadioGroup variants = findViewById(R.id.variants);
+                        int id = variants.getCheckedRadioButtonId();
+                        if (id != -1) {
+                            showAnswer();
+                            position++;
+                            fillForm();
+                        } else {
+                            showAnswer();
+                            fillForm();
+                        }
+                    }
+                }
+        );
+
+        Button previous = findViewById(R.id.previous);
+        previous.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        position--;
+                        fillForm();
+                    }
+                }
+        );
         Log.d(TAG, "Turn Counter: " + String.valueOf(counterTurn));
         Log.d(TAG, "onCreate");
     }
