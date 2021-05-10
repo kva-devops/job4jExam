45ccc5207103f4c4a7bc13221ef00be93c650638
vladimirKutyavin <kva.devops@gmail.com>
2021-05-10 16:13:33 +0400

    2.2. Зависимости компонентов. [#526037]

--- app/src/main/java/ru/job4j/exam/ExamActivity.java
+++ app/src/main/java/ru/job4j/exam/ExamActivity.java
@@ -17,33 +17,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import ru.job4j.exam.store.QuestionStore;
+
 public class ExamActivity extends AppCompatActivity {
 
     private static final String TAG = "ExamActivity";
     private static int counterTurn = 0;
-    private final List<Question> questions = Arrays.asList(
-            new Question(
-                    1, "How many primitive variables does Java have?",
-                    Arrays.asList(
-                            new Option(1, "1.1"), new Option(2, "1.2"),
-                            new Option(3, "1.3"), new Option(4, "1.4")
-                    ), 4
-            ),
-            new Question(
-                    2, "What is Java Virtual Machine?",
-                    Arrays.asList(
-                            new Option(1, "2.1"), new Option(2, "2.2"),
-                            new Option(3, "2.3"), new Option(4, "2.4")
-                    ), 4
-            ),
-            new Question(
-                    3, "What is happen if we try unboxing null?",
-                    Arrays.asList(
-                            new Option(1, "3.1"), new Option(2, "3.2"),
-                            new Option(3, "3.3"), new Option(4, "3.4")
-                    ), 4
-            )
-    );
+    private final QuestionStore store = QuestionStore.getInstance();
     private int position = 0;
     private Map<Integer, Integer> result = new HashMap<>();
 
@@ -51,7 +31,7 @@ public class ExamActivity extends AppCompatActivity {
         findViewById(R.id.previous).setEnabled(position != 0);
         findViewById(R.id.next).setEnabled(false);
         final TextView text = findViewById(R.id.question);
-        Question question = this.questions.get(this.position);
+        Question question = this.store.get(this.position);
         text.setText(question.getText());
         RadioGroup variants = findViewById(R.id.variants);
         variants.setOnCheckedChangeListener(this::radioCheck);
@@ -66,7 +46,7 @@ public class ExamActivity extends AppCompatActivity {
     private void showAnswer() {
         RadioGroup variants = findViewById(R.id.variants);
         int id = variants.getCheckedRadioButtonId();
-        Question question = this.questions.get(this.position);
+        Question question = this.store.get(this.position);
         result.put(position, id);
         Toast.makeText(
                 this, "Your answer is " + id + ", correct is " + question.getAnswer(),
@@ -108,7 +88,7 @@ public class ExamActivity extends AppCompatActivity {
     }
 
     private void radioCheck(RadioGroup radioGroup, int i) {
-        if (position != questions.size() - 1) {
+        if (position != store.size() - 1) {
             findViewById(R.id.next).setEnabled(true);
         }
     }
